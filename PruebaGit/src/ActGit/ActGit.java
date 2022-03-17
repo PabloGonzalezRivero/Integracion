466d551e91402af899ae1a281b0ec03f221ec327
PabloGonzalezRivero <pagonri2002@gmail.com>
2022-03-17 08:47:16 +0100

    Primer commit de la integración Eclipse-GitHub

---
+++ PruebaGit/src/ActGit/ActGit.java
@@ -0,0 +1,12 @@
+package ActGit;
+
+public class ActGit {
+	public static void main(String[] args){
+		System.out.println("Hola mundo"); 
+		System.out.println("Adios mundo"); 
+		System.out.println("Hola de nuevo"); 
+		String nombre = "Pablo González";
+		System.out.println("Adios de nuevo"); 
+		System.out.println("PD: Mi nombre es: " + nombre); 
+	}
+}
